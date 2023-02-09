f1f5ebcec6ca2d19e3be335c4bd962bd1849a0f8
gui-diass <guidiass03@gmail.com>
2023-02-09 13:19:41 -0300

    .

---
+++ presentational-only/presentational-only.js
@@ -0,0 +1,27 @@
+$(document).ready(function()
+{
+  $('#load-more-content').click(function()
+  {
+    
+    $('#more-content').toggleClass('shown');
+
+    $('#load-more-content').hide();
+
+    if( $('#more-content').hasClass('shown') )
+    {
+      $('#load-more-content').text('Esconder campo');
+      $('#more-content').fadeIn('slow', function()
+      {
+        $('#load-more-content').fadeIn('slow');
+      });
+    }
+    else
+    {
+      $('#load-more-content').text('Sobre Mim');
+      $('#more-content').fadeOut('slow', function()
+      {
+        $('#load-more-content').fadeIn('slow');
+      });
+    }
+  });
+});
